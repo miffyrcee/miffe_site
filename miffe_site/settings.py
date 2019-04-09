514a2cca99b76af4bdd4029ffb9077ac31682ef5
miffyrcee <miffyrcee@outlook.com>
2019-04-09 16:44:13 +0800

    update

--- miffe_site/settings.py
+++ miffe_site/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'botf',
     'botb',
+    'django_telegrambot',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'miffe_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['botf/dist'],
+        'DIRS': [os.path.join(BASE_DIR, 'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,6 +86,80 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+DJANGO_TELEGRAMBOT = {
+
+    'MODE': 'WEBHOOK',  # (Optional [str]) # The default value is WEBHOOK,
+    # otherwise you may use 'POLLING'
+                        # NB: if use polling you must provide to run
+                        # a management command that starts a worker
+
+    'WEBHOOK_SITE': 'https://miffe.site.com',
+    # (Optional[str]) # If this value is specified,
+    'WEBHOOK_PREFIX': '/prefix',
+    # a prefix is added to webhook url
+
+    # If your site use self-signed
+    'WEBHOOK_CERTIFICATE': '/etc/nginx/ssl/miffe_site.pem',
+    # certificate, must be set with location of your public key
+    # certificate.(More info at https://core.telegram.org/bots/self-signed )
+
+    'STRICT_INIT': True,  # If set to True, the server will fail to start if some of the
+    # apps contain telegrambot.py files that cannot be successfully
+    # imported.
+
+    'BOTS': [
+        {
+            # Your bot token.
+            'TOKEN': '786276948:AAEwplAQNpcEF5BtObF4dQ6_gnh7gADjJ4w'
+
+            # 'ALLOWED_UPDATES':(Optional[list[str]]), # List the types of
+            # updates you want your bot to receive. For example, specify
+            # ``["message", "edited_channel_post", "callback_query"]`` to
+            # only receive updates of these types. See ``telegram.Update``
+            # for a complete list of available update types.
+            # Specify an empty list to receive all updates regardless of type
+            # (default). If not specified, the previous setting will be used.
+            # Please note that this parameter doesn't affect updates created
+            # before the call to the setWebhook, so unwanted updates may be
+            # received for a short period of time.
+
+            # 'TIMEOUT':(Optional[int|float]), # If this value is specified,
+            # use it as the read timeout from the server
+
+            # 'WEBHOOK_MAX_CONNECTIONS':(Optional[int]), # Maximum allowed number of
+            # simultaneous HTTPS connections to the webhook for update
+            # delivery, 1-100. Defaults to 40. Use lower values to limit the
+            # load on your bot's server, and higher values to increase your
+            # bot's throughput.
+
+            # 'MESSAGEQUEUE_ENABLED':(Optinal[bool]), # Make this True if you want to use messagequeue
+
+            # 'MESSAGEQUEUE_ALL_BURST_LIMIT':(Optional[int]), # If not provided 29 is the default value
+
+            # 'MESSAGEQUEUE_ALL_TIME_LIMIT_MS':(Optional[int]), # If not provided 1024 is the default value
+
+            # 'MESSAGEQUEUE_REQUEST_CON_POOL_SIZE':(Optional[int]), # If not provided 8 is the default value
+
+            # 'POLL_INTERVAL' : (Optional[float]), # Time to wait between polling updates from Telegram in
+            #seconds. Default is 0.0
+
+            # 'POLL_CLEAN':(Optional[bool]), # Whether to clean any pending updates on Telegram servers before
+            # actually starting to poll. Default is False.
+
+            # 'POLL_BOOTSTRAP_RETRIES':(Optional[int]), # Whether the bootstrapping phase of the `Updater`
+            # will retry on failures on the Telegram server.
+            # |   < 0 - retry indefinitely
+            # |     0 - no retries (default)
+            # |   > 0 - retry up to X times
+
+            # 'POLL_READ_LATENCY':(Optional[float|int]), # Grace time in seconds for receiving the reply from
+            # server. Will be added to the `timeout` value and used as the read timeout from
+            # server (Default: 2).
+        },
+        # Other bots here with same structure.
+    ],
+
 }
 
 
@@ -125,7 +200,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'botf/dist/static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "botf/dist/static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#  STATICFILES_DIRS = [
+#  os.path.join(BASE_DIR, "botf/dist/static"),
+#  ]
--- miffe_site/urls.py
+++ miffe_site/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import django_telegrambot
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url('', include('django_telegrambot.urls')),
 ]
