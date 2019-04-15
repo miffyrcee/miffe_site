0da99dbff005e168b32b565b7652ddf9c9a01ee1
miffyrcee <miffyrcee@outlook.com>
2019-04-15 13:53:51 +0800

    update

--- bot/telegrambot.py
+++ bot/telegrambot.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# Example code for telegrambot.py module
 from telegram.ext import CommandHandler, MessageHandler, Filters
 from django_telegrambot.apps import DjangoTelegramBot
 
@@ -21,22 +23,27 @@ def echo(bot, update):
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
-
+    
 
 def main():
     logger.info("Loading handlers for telegram bot")
-
-    # Default dispatcher (this is related to the first bot in settings.DJANGO_TELEGRAMBOT['BOTS'])
+    
+    # Default dispatcher (this is related to the first bot in settings.TELEGRAM_BOT_TOKENS)
     dp = DjangoTelegramBot.dispatcher
     # To get Dispatcher related to a specific bot
     # dp = DjangoTelegramBot.getDispatcher('BOT_n_token')     #get by bot token
     # dp = DjangoTelegramBot.getDispatcher('BOT_n_username')  #get by bot username
-
+    
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler([Filters.text], echo))
 
     # log all errors
     dp.add_error_handler(error)
+
+    # log all errors
+    dp.addErrorHandler(error)
+
