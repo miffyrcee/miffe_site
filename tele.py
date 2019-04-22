a18823a72d1eda7c9aa7bbe977a3596f42dbd05c
miffyrcee <miffyrcee@outlook.com>
2019-04-22 22:56:25 +0800

    update

--- tele.py
+++ tele.py
@@ -34,16 +34,10 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     if (message == "open"):
-        formaters.writeJson('data.json', echoParmers(vara), echoParmers(varb),echoParmers(varc), "1")
-    else if(message == "close"):
-        formaters.writeJson('data.json', echoParmers(vara), echoParmers(varb),echoParmers(varc), "0")
+        formaters.writeJson('data.json', echoParmers(vara), echoParmers(varb),
+                            echoParmers(varc), "1")
+    elif (message == "close"):
+        formaters.writeJson('data.json', echoParmers(vara), echoParmers(varb),
+                            echoParmers(varc), "0")
     else:
         bot.reply_to(message, message.text)
-
-
-bot.polling()
-markup = types.ReplyKeyboardMarkup(row_width=2)
-itembtn1 = types.KeyboardButton('open')
-itembtn2 = types.KeyboardButton('close')
-markup.add(itembtn1, itembtn2)
-tb.send_message(chat_id, "Choose the open or close", reply_markup=markup)
